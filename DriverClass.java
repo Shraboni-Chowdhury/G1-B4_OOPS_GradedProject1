6eb5d641e67894d4e2e3e160148b1b996c54895d
Shraboni-Chowdhury <119192588+Shraboni-Chowdhury@users.noreply.github.com>
2022-12-05 22:57:10 +0530

    Add files via upload

---
+++ DriverClass.java
@@ -0,0 +1,66 @@
+package com.gl.main;
+
+import java.util.Scanner;
+
+import com.gl.model.Employee;
+
+public class DriverClass {
+	
+	public static void main(String[] args) {
+		
+		Employee employee = new Employee("Harshit","Choudary");
+		
+		ICredentials cs = new CredentialService;
+		
+		
+		System.out.println("Please enter the department from the following ");
+		System.out.println("1. Technical");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. Legal");
+		
+		Scanner sc = new Scanner(System.in);
+		int choice = sc.nextInt();
+		
+		String generatedEmail = null;
+		String generatedPassword = null;
+		
+		switch (choice) {
+		
+		case 1:{
+		 generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+				 employee.getLastName().toLowerCase(), "tech");
+		 generatedPassword = cs.generatePassword();
+		 break;
+		   }
+		
+		case 2:{
+			 generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					 employee.getLastName().toLowerCase(), "admin");
+			 generatedPassword = cs.generatePassword();
+			 break;
+			}
+		case 3:{
+			 generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					 employee.getLastName().toLowerCase(), "hr");
+			 generatedPassword = cs.generatePassword();
+			 break;
+			}
+		case 4:{
+			 generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					 employee.getLastName().toLowerCase(), "lg");
+			 generatedPassword = cs.generatePassword();
+			 break;
+			}
+		
+		default:{
+			System.out.println("Enter a valid option");
+		}
+		}
+		
+		employee.setEmail(generatedEmail);
+		employee.setPassword(generatedPassword);
+		cs.showCredentials(employee);
+		sc.close();
+	}
+}
